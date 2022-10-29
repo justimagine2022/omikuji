72c68267cfa0f096ad2321ac0ad12308aaea8705
ばうむ <97399220+justimagine2022@users.noreply.github.com>
2022-10-29 22:04:16 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+
+  result.textContent = '';
+
+  btn.addEventListener('click', () => {
+    const results = ['大吉', '吉', '中吉', '末吉', '小吉', '凶', '大凶'];
+    const n = Math.floor(Math.random() * results.length);
+    result.textContent = results[n];
+  });
+}
