d2d3197f62ba8c70590fee77f1d9fa9dc87284c0
ばうむ <97399220+justimagine2022@users.noreply.github.com>
2023-03-29 23:59:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@
 {
   const btn = document.getElementById('btn');
   const result = document.getElementById('result');
-
-  result.textContent = '';
+  const results = ['大吉', '小吉', '中吉', '吉', '末吉', '凶', '大凶'];
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '吉', '中吉', '末吉', '小吉', '凶', '大凶'];
-    const n = Math.floor(Math.random() * results.length);
-    result.textContent = results[n];
+    const n = Math.random(); //0以上1未満の数を生成する
+    if (n < 0.05) {  //確率5%
+      result.textContent = results[6];
+    } else if (n < 0.2) {  //確率15% 20-5=15
+      result.textContent = results[5];
+    } else {  //確率80%  100-15-5=80
+      result.textContent = results[Math.floor(Math.random() * 5)];
+    }
+    
   });
 }
