8425ee80852c841a836dd7bee5bb49314f1b6b70
ばうむ <97399220+justimagine2022@users.noreply.github.com>
2023-03-30 00:19:10 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@
   const results = ['大吉', '小吉', '中吉', '吉', '末吉', '凶', '大凶'];
 
   btn.addEventListener('click', () => {
-    const n = Math.random(); //0以上1未満の数を生成する
-    if (n < 0.05) {  //確率5%
+    const n = Math.random(); //0以上1未満の数を生成する0.
+    if (n < 0.01) {  //確率1%
       result.textContent = results[6];
-    } else if (n < 0.2) {  //確率15% 20-5=15
+    } else if (n < 0.05) {  //確率4% 5-1=4
       result.textContent = results[5];
-    } else {  //確率80%  100-15-5=80
+    } else {  //確率95%  100-4-1=95
       result.textContent = results[Math.floor(Math.random() * 5)];
     }
     
