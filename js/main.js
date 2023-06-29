5394ed465c09ffefb39ac78c6e002012118b8143
ばうむ <97399220+justimagine2022@users.noreply.github.com>
2023-06-29 23:48:23 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 'use strict';
 
 {
-  const btn = document.getElementById('btn');
-  const result = document.getElementById('result');
-  const results = ['大吉', '小吉', '中吉', '吉', '末吉', '凶', '大凶'];
+const button = document.querySelector('button');
+const result = document.querySelector('p');
 
-  btn.addEventListener('click', () => {
-    const n = Math.random(); //0以上1未満の数を生成する0.
-    if (n < 0.01) {  //確率1%
-      result.textContent = results[6];
-    } else if (n < 0.05) {  //確率4% 5-1=4
-      result.textContent = results[5];
-    } else {  //確率95%  100-4-1=95
-      result.textContent = results[Math.floor(Math.random() * 5)];
-    }
-    
-  });
+const results =['大凶', '大吉', '吉', '凶', '中吉', '小吉', '末吉'];
+
+function draw() {
+  const n = Math.random();
+  const m = Math.floor(Math.random() * 4) + 3;
+  
+  if (n < 0.01) {
+    result.textContent = results[0];
+  } else if (n < 0.16) {
+    result.textContent = results[1];
+  } else if (n < 0.35) {
+    result.textContent = results[2];
+  } else if (n < 0.29) {
+    result.textContent = results[3];
+  } else {
+    result.textContent = results[m];
+  }
+}
+
+button.addEventListener('click', () => {
+  draw();
+});
+
 }
